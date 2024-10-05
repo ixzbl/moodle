4746114d246c642b81a64a5977708f38582ec833
Jun Pataleta <jun@moodle.com>
2024-10-05 15:08:28 +0800

    MDL-82173 tool_task: Allow filtering by task ID and customdata

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024100400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2024100500;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.5rc2 (Build: 20241004)';    // Human-friendly version name
-$branch   = '405';                      // This version's branch.
-$maturity = MATURITY_RC;                // This version's maturity level.
+$release  = '5.0dev (Build: 20241007)';    // Human-friendly version name
+$branch   = '500';                      // This version's branch.
+$maturity = MATURITY_ALPHA;             // This version's maturity level.
